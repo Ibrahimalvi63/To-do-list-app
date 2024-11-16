b721056657b9ed3c3377e19ac84758537ea82aef
Ibrahim Alvi <164319926+Ibrahimalvi63@users.noreply.github.com>
2024-11-17 00:22:10 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const btnEl = document.querySelector("button");
+
+const inputEl = document.querySelector("#input_box");
+
+
+const taskList = document.querySelector('#task-list');
+
+
+
+
+
+
+
+
+btnEl.addEventListener('click', ()=>{
+
+    if(inputEl.value === ''){
+        alert('You will write something.')
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputEl.value;
+        taskList.appendChild(li);
+        let crossIcon = document.createElement('span');
+        crossIcon.innerHTML = '\u00d7';
+        li.appendChild(crossIcon)
+    }
+    inputEl.value = '';
+    saveData();
+})
+
+taskList.addEventListener('click',(e)=>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+
+function saveData() {
+    localStorage.setItem('data', taskList.innerHTML);
+    
+}
+
+function showData() {
+    taskList.innerHTML = localStorage.getItem('data');
+    
+}
+
+showData();
